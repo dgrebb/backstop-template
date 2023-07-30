c2c1d6a510b91e15c553fe83209e7bff59faefb9
Dan Grebb <dan@dgrebb.com>
2023-07-30 12:16:58 -0400

    fix(project): adjusts paths and updates npm scripts to use path for backstop project

--- _ci/backstop/scenarios/index.js
+++ _ci/backstop/scenarios/index.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 
 // Read in the libs from this directory and add them as exports
 // This way you can just reference
-fs.readdirSync("./backstop/scenarios").forEach(function(file) {
+fs.readdirSync("./_ci/backstop/scenarios").forEach(function(file) {
   console.log(file);
   if (file.indexOf(".js") > -1 && file != "index.js")
     exports[file.replace(".js", "")] = require("./" + file);
